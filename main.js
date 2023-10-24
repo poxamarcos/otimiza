c687d79f057893b74d6b69b41caf5a165994db4b
Marcos Aurélio de Souto Filho <88385200+poxamarcos@users.noreply.github.com>
2023-10-24 14:56:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const diaDoEvento = new Date('Sep 30 2023 00:00:00')
-    const timeSeconds = diaDoEvento.getTime()
+var el = document.querySelector("#text");
+var text = "Deixe que cuidemos daqueles que mais importam para você, seus leads."
 
-    const time = setInterval(() => {
-        const data = new Date()
-        const timeSecondsData = data.getTime()
 
-        const diasAteExpirar = timeSeconds - timeSecondsData
+var interval = 65;
+function showtext(el, text, interval) {
+  var char = text.split("").reverse();
+  
+  var typer = setInterval(function() {
+  
+    if (!char.length) {
+        return clearInterval(typer)
+    }
+    
+    var next = char.pop();
+    
+    el.innerHTML += next;
+    
+  }, interval);
+  
+}
+showtext(el, text, interval);
 
-        const dias = Math.floor(diasAteExpirar / (1000 * 60 * 60 * 24))
-        const horas = Math.floor(diasAteExpirar % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
-        const minutos = Math.floor(diasAteExpirar % (1000 * 60 * 60) / (1000 * 60))
-        const secundos = Math.floor(diasAteExpirar % (1000 * 60) / 1000)
-
-        document.querySelector('#time-da-promocao').innerHTML = `${dias}d ${horas}h ${minutos}m ${secundos}s`
-
-        if (diasAteExpirar < 0) {
-            clearInterval(time)
-            document.querySelector('#time-da-promocao').innerHTML = 'A Promoção terminou'
-            document.querySelector('#pre-text').innerHTML = ''
-        }
-
-    })
-})
 
